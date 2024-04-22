464e2270684fb269dbdd25c6eb051c97a7447477
Diogo Martins <boraladiogo@gmail.com>
2024-04-22 11:16:14 -0300

    Add exercises about quantifiers

---
+++ explicit-characters-and-quantifiers/exercise.js
@@ -0,0 +1,17 @@
+// Write your regular expressions within the // delimiters. Example: /a+/
+
+// Exercise 1: Match the string yeehaw with 2 or more es after the y,
+// and a single optional exclamation point (!) at the end
+const yeehawRegex = /yee+haw!?/;
+
+// Exercise 2: Match the word “hi”, followed by two or more s’s
+// what's the most readable way to write this?
+const hissRegex = /hiss+/;
+
+// Exercise 3: Match the word “Yay” with three, four,
+// five or six exclamation points at the end
+const yayRegex = /Yay!{3,6}/;
+
+// Exercise 4: Match “Call me maybe” with any number of ?s
+// at the end (including zero)
+const callRegex = /Call me maybe\?*/;
